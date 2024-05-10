d3bb53934ac6a520899e2ccb6db41dce8ac66e82
MikaelaGisbert <karlamikaela.911@gmail.com>
2024-05-10 10:15:17 -0400

    Solucion

---
+++ src/components/TextoEjercicio.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextoEjercicio = () =>{
+
+    return(
+        <div className="ContenerdorDescripcion">
+            <h1 className="Titulo">Terapia 2</h1>
+            <p className="Descripcion">Ejercicios pelvicos</p>
+        </div>
+    );
+}
